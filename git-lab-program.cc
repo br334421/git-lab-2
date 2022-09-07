a1e050e9747b26824ad025da429d46b5a8280184
Braeden Rodgers <br334421@ohio.edu>
2022-09-07 16:28:22 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*       File: git-lab-program.cc
+*     Author: Braeden Rodgers
+*       Date: 9/6/22
+*Description:
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!" << endl;
+
+    return 0;
+}
